659df71fd439506a852a9ee963753861b5c17026
kamana082230 <kamana082230@gmail.com>
2026-03-04 15:04:42 +0545

    add postcss and tailwind configuration files

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,jsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#FF6B6B',
+        secondary: '#4ECDC4',
+        accent: '#FFE66D',
+        dark: '#2D3436',
+      }
+    },
+  },
+  plugins: [],
+}
